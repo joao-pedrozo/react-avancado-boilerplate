2c716fe3fbf6fe2665a6a215672faef72477a089
João Pedrozo <jp.pedrozo@hotmail.com>
2020-11-19 20:09:24 -0300

    create secondary storybook

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,3 +1,4 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
 import { withKnobs, text } from '@storybook/addon-knobs';
 import Main from '.';
 
@@ -5,11 +6,21 @@ export default {
     title: 'Main',
     component: Main,
     decorators: [withKnobs]
-};
+} as Meta;
 
-export const Basic = () => (
+export const Basic: Story = () => (
     <Main
         title={text('Title', 'React avançado123')}
+        description={text(
+            'Description',
+            'A random description wich you can change anytime you want!'
+        )}
+    />
+);
+
+export const Secondary: Story = () => (
+    <Main
+        title={text('Title', 'React avançado123123')}
         description={text(
             'Description',
             'A random description wich you can change anytime you want!'
