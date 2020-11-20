e914f25d0e4d4578b99bb6a4d5af94593e08e8b6
João Pedrozo <jp.pedrozo@hotmail.com>
2020-11-20 11:04:54 -0300

    fix: corrige cobertura de testes

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
     testeEnviroment: 'jsdom',
     testPathIgnorePatterns: ['/node_modules/', '/.next/'],
     collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.ts(x)'],
+    collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
     setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'] //Antes de todos os testes vai rodar eses arquivo
 };
