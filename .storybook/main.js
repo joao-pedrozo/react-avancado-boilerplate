0a64af983e19020c6deaadae0cb30177161b6beb
João Pedrozo <jp.pedrozo@hotmail.com>
2020-11-19 22:12:02 -0300

    finish boilerplate creation, configure pwa and other next stuff not related in this commit message

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,8 @@
 module.exports = {
     stories: ['../src/components/**/stories.tsx'],
-    addons: ['@storybook/addon-essentials', '@storybook/addon-knobs','@storybook/addon-actions', '@storybook/addon-links',],
+    addons: [
+        '@storybook/addon-essentials', 
+    ],
     webpackFinal: (config) => {
       config.resolve.modules.push(`${process.cwd()}/src`)
       return config
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,29 +1,10 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { withKnobs, text } from '@storybook/addon-knobs';
 import Main from '.';
 
 export default {
     title: 'Main',
-    component: Main,
-    decorators: [withKnobs]
+    component: Main
 } as Meta;
 
-export const Basic: Story = () => (
-    <Main
-        title={text('Title', 'React avançado123')}
-        description={text(
-            'Description',
-            'A random description wich you can change anytime you want!'
-        )}
-    />
-);
-
-export const Secondary: Story = () => (
-    <Main
-        title={text('Title', 'React avançado123123')}
-        description={text(
-            'Description',
-            'A random description wich you can change anytime you want!'
-        )}
-    />
-);
+export const Basic: Story = (args) => <Main {...args} />;
+export const Secondary: Story = (args) => <Main {...args} />;
